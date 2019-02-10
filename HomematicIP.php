f7b766e13ff6c40e07a7d46ade4dc5a49142a5fe
Tobias Setzekorn <ecom@tssn.de>
2019-02-10 10:55:56 +0100

    Bugfix Include

--- HomematicIP.php
+++ HomematicIP.php
@@ -1,5 +1,5 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    include_once('classes/' . $class_name . '.php');
+    include_once(__DIR__ . '/classes/' . $class_name . '.php');
 });
 ?> 
